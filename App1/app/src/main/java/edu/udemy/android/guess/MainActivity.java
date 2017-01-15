bc6a3f4e1a6b510f3d96e7cd88a07d1693bbf175
TomazMartins <tomaz.r.martins@gmail.com>
2017-01-15 10:59:56 -0200

    Implementing method onClick()
    Signed-off-by: TomazMartins <tomaz.r.martins@gmail.com>

--- App1/app/src/main/java/edu/udemy/android/guess/MainActivity.java
+++ App1/app/src/main/java/edu/udemy/android/guess/MainActivity.java
@@ -3,12 +3,29 @@ package edu.udemy.android.guess;
 import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends Activity {
+
+    private Button btn_play;
+    private TextView txv_result;
 
     @Override
     protected void onCreate( Bundle savedInstanceState ) {
         super.onCreate( savedInstanceState );
         setContentView( R.layout.activity_main );
+
+        btn_play = (Button) findViewById( R.id.btn_play );
+        txv_result = (TextView) findViewById( R.id.txv_result );
+
+        btn_play.setOnClickListener( new View.OnClickListener() {
+
+            @Override
+            public void onClick( View v ) {
+                txv_result.setText( "Result: changed text" );
+            }
+        });
     }
 }
