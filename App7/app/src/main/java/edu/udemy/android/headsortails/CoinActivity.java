a99ad20dddbb4a679595ba7af5d3c1f8d5ab579b
TomazMartins <tomaz.r.martins@gmail.com>
2017-01-20 23:06:07 -0200

    Ending App7
    Signed-off-by: TomazMartins <tomaz.r.martins@gmail.com>

--- App7/app/src/main/java/edu/udemy/android/headsortails/CoinActivity.java
+++ App7/app/src/main/java/edu/udemy/android/headsortails/CoinActivity.java
@@ -1,13 +1,46 @@
 package edu.udemy.android.headsortails;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.media.Image;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
 public class CoinActivity extends AppCompatActivity {
+
+    private Button btn_back;
+    private ImageView img_coin;
 
     @Override
     protected void onCreate( Bundle savedInstanceState ) {
         super.onCreate( savedInstanceState );
         setContentView( R.layout.activity_coin );
+
+        btn_back = (Button) findViewById( R.id.btn_back );
+        img_coin = (ImageView) findViewById( R.id.img_coin );
+
+        Bundle extra = getIntent().getExtras();
+        String chosenOption = "";
+
+        if( extra != null ) {
+            chosenOption = extra.getString( "result" );
+        }
+
+        if( chosenOption.equals( "Head" ) ) {
+            img_coin.setImageDrawable( ContextCompat.getDrawable( this, R.drawable.coin_face ) );
+        } else if( chosenOption.equals( "Tail" ) ) {
+            img_coin.setImageDrawable( ContextCompat.getDrawable( this, R.drawable.coin_value ) );
+        }
+
+        btn_back.setOnClickListener( new View.OnClickListener() {
+            @Override
+            public void onClick( View view ) {
+                startActivity( new Intent( CoinActivity.this, MainActivity.class ) );
+            }
+        } );
     }
 }
--- App7/app/src/main/java/edu/udemy/android/headsortails/MainActivity.java
+++ App7/app/src/main/java/edu/udemy/android/headsortails/MainActivity.java
@@ -1,13 +1,58 @@
 package edu.udemy.android.headsortails;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
+
+    public enum Option {
+        HEADS( "Head" ),
+        TAILS( "Tail" );
+
+        private String name;
+
+        Option( String name ) {
+            this.name = name;
+        }
+
+        public String getName() {
+            return this.name;
+        }
+    }
+
+    private Button btn_play;
 
     @Override
     protected void onCreate( Bundle savedInstanceState ) {
         super.onCreate( savedInstanceState );
         setContentView( R.layout.activity_main );
+
+        btn_play = (Button) findViewById( R.id.btn_play );
+
+        btn_play.setOnClickListener( new View.OnClickListener() {
+            Random random = new Random();
+            int randomNumber = random.nextInt( 2 );
+
+            @Override
+            public void onClick( View view ) {
+                Intent startCoinActivity = new Intent( MainActivity.this, CoinActivity.class );
+
+                switch( randomNumber ) {
+                    case 0:
+                        startCoinActivity.putExtra( "result", Option.HEADS.getName() );
+                        break;
+                    case 1:
+                        startCoinActivity.putExtra( "result", Option.TAILS.getName() );
+                }
+
+                startActivity( startCoinActivity );
+            }
+        } );
     }
 }
