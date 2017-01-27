d3b16c695de8e19e4db9e7cfc671b8cf618e8d26
TomazMartins <tomaz.r.martins@gmail.com>
2017-01-27 15:23:39 -0200

    Initial app12
    This app is to learn more about Medias
    
    Signed-off-by: TomazMartins <tomaz.r.martins@gmail.com>

---
+++ learning-media2/app/src/main/java/edu/udemy/android/soundofanimals/MainActivity.java
@@ -0,0 +1,13 @@
+package edu.udemy.android.soundofanimals;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate( Bundle savedInstanceState ) {
+        super.onCreate( savedInstanceState );
+        setContentView( R.layout.activity_main );
+    }
+}
