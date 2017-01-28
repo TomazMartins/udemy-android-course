abef9b0afd7488e0487679f5fe2cc9237f351cd1
TomazMartins <tomaz.r.martins@gmail.com>
2017-01-28 16:17:31 -0200

    Improving Knowledge about SQLite
    Signed-off-by: TomazMartins <tomaz.r.martins@gmail.com>

--- learning-sqlite/app/src/main/java/edu/udemy/android/sqlite/MainActivity.java
+++ learning-sqlite/app/src/main/java/edu/udemy/android/sqlite/MainActivity.java
@@ -9,8 +9,12 @@ import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private static final int EMPTY = 0;
+
     private Button btn_show;
-    private TextView txv_show;
+    private Button btn_insert;
+    private Button btn_delete;
+    private TextView txv_text;
 
     private SQLiteDatabase database;
     private Cursor cursor;
@@ -25,21 +29,35 @@ public class MainActivity extends AppCompatActivity {
         // Create Table
         database.execSQL( "CREATE TABLE IF NOT EXISTS people( name VACHAR, age INT(3) ) " );
 
-        //Insert Table
-        database.execSQL( "INSERT INTO people ( name, age ) VALUES ( 'Thaiane', 24 )" );
-        database.execSQL( "INSERT INTO people ( name, age ) VALUES ( 'Tomaz', 29 )" );
-        database.execSQL( "INSERT INTO people ( name, age ) VALUES ( 'Thaiane e Tomaz', 7 )" );
-
 
         btn_show = (Button) findViewById( R.id.btn_show );
-        txv_show = (TextView) findViewById( R.id.txv_show );
 
         btn_show.setOnClickListener( new View.OnClickListener() {
             @Override
             public void onClick( View view ) {
-                travelDatabase( database );
+                travelDatabase();
             }
         } );
+
+        btn_insert = (Button) findViewById( R.id.btn_insert );
+
+        btn_insert.setOnClickListener( new View.OnClickListener() {
+            @Override
+            public void onClick( View view ) {
+                insertData();
+            }
+        } );
+
+        btn_delete = (Button) findViewById( R.id.btn_delete );
+
+        btn_delete.setOnClickListener( new View.OnClickListener() {
+            @Override
+            public void onClick( View v ) {
+                deleteData();
+            }
+        } );
+
+        txv_text = (TextView) findViewById( R.id.txv_text );
     }
 
     @Override
@@ -50,52 +68,80 @@ public class MainActivity extends AppCompatActivity {
         super.onDestroy();
     }
 
-    private void travelDatabase( SQLiteDatabase database ) {
-        cursor = database.rawQuery( "SELECT name, age FROM people", null );
-
-        cursor.moveToFirst();
-
-        String result = "";
-
-        // Iterate over Rows
-        //
-        for( int i = 0; i < cursor.getCount(); i++ ) {
-
-            // Iterate over Columns
-            //
-            for( int j = 0; j < cursor.getColumnNames().length; j++ ) {
-                result += cursor.getString( j ) + " ";
-            }
-
-            cursor.moveToNext();
-
-            result += "\n";
+    private void deleteData() {
+        if( database.isOpen() ) {
+            database.execSQL( "DELETE FROM people" );
+        } else {
+            txv_text.setText( R.string.db_not_opened );
         }
-
-        txv_show.setText( result );
     }
+
+    private void insertData() {
+        if( database.isOpen() ) {
+            database.execSQL( "INSERT INTO people ( name, age ) VALUES ( 'Thaiane', 24 )" );
+            database.execSQL( "INSERT INTO people ( name, age ) VALUES ( 'Tomaz', 29 )" );
+            database.execSQL( "INSERT INTO people ( name, age ) VALUES ( 'Thaiane e Tomaz', 7 )" );
+        } else {
+            txv_text.setText( R.string.db_not_opened );
+        }
+    }
+
+    private void travelDatabase() {
+        if( database.isOpen() ) {
+            cursor = database.rawQuery( "SELECT name, age FROM people", null );
+
+            int indexColumnName = cursor.getColumnIndex( "name" );
+            int indexColumnAge = cursor.getColumnIndex( "age" );
+
+            cursor.moveToFirst();
+
+            String name;
+            String age;
+            String result = "";
+
+            if( cursor.getCount() != EMPTY ) {
+                do {
+                    name = cursor.getString( indexColumnName );
+                    age = cursor.getString( indexColumnAge );
+
+                    result += name;
+                    result += " ";
+                    result += age;
+                    result += "\n";
+                } while( cursor.moveToNext() );
+
+                txv_text.setText( result );
+            } else {
+                txv_text.setText( R.string.db_empty );
+            }
+        } else {
+            txv_text.setText( R.string.db_not_opened );
+        }
+    }
+
+
 
 //    // Another form to travel over Database
 //    //
 //    private void travelDatabase( SQLiteDatabase database ) {
 //        cursor = database.rawQuery( "SELECT name, age FROM people", null );
 //
-//        int indexColumnName = cursor.getColumnIndex( "name" );
-//        int indexColumnAge = cursor.getColumnIndex( "age" );
-//
 //        cursor.moveToFirst();
 //
-//        String name;
-//        String age;
 //        String result = "";
 //
-//        while( cursor.moveToNext() ) {
-//            name = cursor.getString( indexColumnName );
-//            age = cursor.getString( indexColumnAge );
+//        // Iterate over Rows
+//        //
+//        for( int i = 0; i < cursor.getCount(); i++ ) {
 //
-//            result += name;
-//            result += " ";
-//            result += age;
+//            // Iterate over Columns
+//            //
+//            for( int j = 0; j < cursor.getColumnNames().length; j++ ) {
+//                result += cursor.getString( j ) + " ";
+//            }
+//
+//            cursor.moveToNext();
+//
 //            result += "\n";
 //        }
 //
