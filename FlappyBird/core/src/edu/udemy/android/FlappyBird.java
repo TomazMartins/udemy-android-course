747257a2bbe0a50d845af339db05b9708f6ccd04
TomazMartins <tomaz.r.martins@gmail.com>
2017-02-04 21:27:46 -0200

    Implemeting moviment and inserting images
    Signed-off-by: TomazMartins <tomaz.r.martins@gmail.com>

--- FlappyBird/core/src/edu/udemy/android/FlappyBird.java
+++ FlappyBird/core/src/edu/udemy/android/FlappyBird.java
@@ -7,27 +7,35 @@ import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 public class FlappyBird extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
-	
-	@Override
-	public void create () {
-		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
-	}
+    private SpriteBatch batch;
 
-	@Override
-	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
-	}
-	
-	@Override
-	public void dispose () {
-		batch.dispose();
-		img.dispose();
-	}
+    private Texture bird;
+    private Texture background;
+
+    private int movement = 0;
+    private int maxWidth = 0;
+    private int maxHeight = 0;
+
+    @Override
+    public void create() {
+        maxWidth = Gdx.graphics.getWidth();
+        maxHeight = Gdx.graphics.getHeight();
+
+        batch = new SpriteBatch();
+
+        bird = new Texture( "bird_1.png" );
+        background = new Texture( "background.png" );
+    }
+
+    @Override
+    public void render() {
+        movement++;
+
+        batch.begin();
+
+        batch.draw( background, 0, 0, maxWidth, maxHeight );
+        batch.draw( bird, movement, 400 );
+
+        batch.end();
+    }
 }
