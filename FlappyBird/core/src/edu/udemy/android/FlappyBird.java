5dfbd396caa7fa3107c5836adbab816e1d60e4d4
TomazMartins <tomaz.r.martins@gmail.com>
2017-02-06 21:29:25 -0200

    Learning to animate objects
    Signed-off-by: TomazMartins <tomaz.r.martins@gmail.com>

--- FlappyBird/core/src/edu/udemy/android/FlappyBird.java
+++ FlappyBird/core/src/edu/udemy/android/FlappyBird.java
@@ -2,19 +2,23 @@ package edu.udemy.android;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 public class FlappyBird extends ApplicationAdapter {
     private SpriteBatch batch;
 
-    private Texture bird;
+    private Texture[] birds;
     private Texture background;
 
     private int movement = 0;
     private int maxWidth = 0;
     private int maxHeight = 0;
+
+    private double variation = 0;
+    private float fallSpeed = 0;
+    private float positionY = 0;
+    private float positionX = 0;
 
     @Override
     public void create() {
@@ -23,18 +27,33 @@ public class FlappyBird extends ApplicationAdapter {
 
         batch = new SpriteBatch();
 
-        bird = new Texture( "bird_1.png" );
+        birds = new Texture[ 3 ];
+        birds[ 0 ] = new Texture("bird_1.png");
+        birds[ 1 ] = new Texture("bird_2.png");
+        birds[ 2 ] = new Texture("bird_3.png");
+
         background = new Texture( "background.png" );
+
+        positionY = maxHeight/2;
     }
 
     @Override
     public void render() {
-        movement++;
+        variation += Gdx.graphics.getDeltaTime() * 10;
+        fallSpeed++;
+
+        if( variation > 2 ) {
+            variation = 0;
+        }
+
+        if( positionY > 0 ) {
+            positionY -= fallSpeed;
+        }
 
         batch.begin();
 
         batch.draw( background, 0, 0, maxWidth, maxHeight );
-        batch.draw( bird, movement, 400 );
+        batch.draw( birds[ (int) variation ], positionX, positionY );
 
         batch.end();
     }
