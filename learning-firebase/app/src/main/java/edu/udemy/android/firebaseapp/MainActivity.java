1f8fbbd3921aa6a7ea1e02a61ed9efeb369c45e1
TomazMartins <tomaz.r.martins@gmail.com>
2017-02-18 18:37:26 -0200

    Implementing methods to create user and elements
    Signed-off-by: TomazMartins <tomaz.r.martins@gmail.com>

--- learning-firebase/app/src/main/java/edu/udemy/android/firebaseapp/MainActivity.java
+++ learning-firebase/app/src/main/java/edu/udemy/android/firebaseapp/MainActivity.java
@@ -2,9 +2,13 @@ package edu.udemy.android.firebaseapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -31,18 +35,54 @@ public class MainActivity extends AppCompatActivity {
     private DatabaseReference usersReference = firebaseReference.child( "users" );
 
 
+    private EditText edt_firstName;
+    private EditText edt_lastName;
+    private EditText edt_age;
+    private EditText edt_sex;
+
+    private TextView txv_show;
+
+    private Button btn_save;
+    private Button btn_show;
+
+    private User currentUser;
+
 
     @Override
     protected void onCreate( Bundle savedInstanceState ) {
         super.onCreate( savedInstanceState );
         setContentView( R.layout.activity_main );
+        create_components();
 
+        btn_save.setOnClickListener( new View.OnClickListener() {
+            @Override
+            public void onClick( View v ) {
+                currentUser = createUser();
+            }
+        } );
+    }
+
+
+    private User createUser() {
         User user = new User();
-        user.setFirstName( "Ana Helena" );
-        user.setLastName( "Araújo" );
-        user.setSex( "Female" );
-        user.setAge( 18 );
 
-        usersReference.child( "001" ).setValue( user );
+        user.setFirstName( edt_firstName.getText().toString() );
+        user.setLastName( edt_lastName.getText().toString() );
+        user.setAge( Integer.parseInt( edt_age.getText().toString() ) );
+        user.setSex( edt_sex.getText().toString() );
+
+        return user;
+    }
+
+    private void create_components() {
+        edt_firstName = (EditText) findViewById( R.id.edt_firstName );
+        edt_lastName = (EditText) findViewById( R.id.edt_lastName );
+        edt_age = (EditText) findViewById( R.id.edt_age );
+        edt_sex = (EditText) findViewById( R.id.edt_sex );
+
+        txv_show = (TextView) findViewById( R.id.txv_show );
+
+        btn_save = (Button) findViewById( R.id.btn_save );
+        btn_show= (Button) findViewById( R.id.btn_show );
     }
 }
