8c5aaeec5a5455126399e517b88fc3fd8f2013c6
TomazMartins <tomaz.r.martins@gmail.com>
2017-02-19 21:21:55 -0300

    Adding masks in editable fields
    Signed-off-by: TomazMartins <tomaz.r.martins@gmail.com>

--- Whatsapp_clone/app/src/main/java/edu/udemy/android/whatsapp_clone/activity/LoginActivity.java
+++ Whatsapp_clone/app/src/main/java/edu/udemy/android/whatsapp_clone/activity/LoginActivity.java
@@ -2,14 +2,51 @@ package edu.udemy.android.whatsapp_clone.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
+
+import com.github.rtoshiro.util.format.SimpleMaskFormatter;
+import com.github.rtoshiro.util.format.text.MaskTextWatcher;
 
 import edu.udemy.android.whatsapp_clone.R;
 
 public class LoginActivity extends AppCompatActivity {
+    private EditText edt_loginPhone;
+    private EditText edt_loginCod;
+    private EditText edt_loginDDD;
 
     @Override
     protected void onCreate( Bundle savedInstanceState ) {
         super.onCreate( savedInstanceState );
         setContentView( R.layout.activity_login );
+
+        createComponents();
+        setMaskFormatter();
+
+    }
+
+
+    private void createComponents() {
+        edt_loginPhone = (EditText) findViewById( R.id.edt_loginPhone );
+        edt_loginCod = (EditText) findViewById( R.id.edt_loginCod );
+        edt_loginDDD = (EditText) findViewById( R.id.edt_loginDDD );
+    }
+
+    private void setMaskFormatter() {
+        /*
+        * In here we add a mask for field of phone.
+        *   For this, we use the lib 'MaskFormatter'. To add this dependency
+        *   follow the instructions in here - https://github.com/rtoshiro/MaskFormatter.
+        * */
+        SimpleMaskFormatter maskPhone = new SimpleMaskFormatter( "NNNNN-NNNN" );
+        MaskTextWatcher maskWatcherPhone = new MaskTextWatcher( edt_loginPhone, maskPhone );
+        edt_loginPhone.addTextChangedListener( maskWatcherPhone );
+
+        SimpleMaskFormatter maskCod = new SimpleMaskFormatter( "+NN" );
+        MaskTextWatcher maskWatcherCod = new MaskTextWatcher( edt_loginCod, maskCod );
+        edt_loginCod.addTextChangedListener( maskWatcherCod );
+
+        SimpleMaskFormatter maskDDD = new SimpleMaskFormatter( "NN" );
+        MaskTextWatcher maskWatcherDDD = new MaskTextWatcher( edt_loginDDD, maskDDD );
+        edt_loginDDD.addTextChangedListener( maskWatcherDDD );
     }
 }
--- Whatsapp_clone/app/src/main/java/edu/udemy/android/whatsapp_clone/activity/ValidatorActivity.java
+++ Whatsapp_clone/app/src/main/java/edu/udemy/android/whatsapp_clone/activity/ValidatorActivity.java
@@ -2,14 +2,38 @@ package edu.udemy.android.whatsapp_clone.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
+
+import com.github.rtoshiro.util.format.SimpleMaskFormatter;
+import com.github.rtoshiro.util.format.text.MaskTextWatcher;
 
 import edu.udemy.android.whatsapp_clone.R;
 
 public class ValidatorActivity extends AppCompatActivity {
+    private EditText edt_validatorNumber;
 
     @Override
     protected void onCreate( Bundle savedInstanceState ) {
         super.onCreate( savedInstanceState );
         setContentView( R.layout.activity_validator );
+
+        createComponents();
+        setMaskFormatter();
+    }
+
+
+    private void createComponents() {
+        edt_validatorNumber = (EditText) findViewById( R.id.edt_validatorNumber );
+    }
+
+    private void setMaskFormatter() {
+        /*
+        * In here we add a mask for field of phone.
+        *   For this, we use the lib 'MaskFormatter'. To add this dependency
+        *   follow the instructions in here - https://github.com/rtoshiro/MaskFormatter.
+        * */
+        SimpleMaskFormatter maskValidator = new SimpleMaskFormatter( "NNNNNN" );
+        MaskTextWatcher maskWatcherValidator = new MaskTextWatcher( edt_validatorNumber, maskValidator );
+        edt_validatorNumber.addTextChangedListener( maskWatcherValidator );
     }
 }
