e9941231f7dda74f3bd6b44d2c74796df37d3a20
TomazMartins <tomaz.r.martins@gmail.com>
2017-02-25 10:59:02 -0300

    Implementing authentication of users
    Signed-off-by: TomazMartins <tomaz.r.martins@gmail.com>

--- Whatsapp_clone/app/src/main/java/edu/udemy/android/whatsapp_clone/activity/RegisterUserActivity.java
+++ Whatsapp_clone/app/src/main/java/edu/udemy/android/whatsapp_clone/activity/RegisterUserActivity.java
@@ -1,15 +1,92 @@
 package edu.udemy.android.whatsapp_clone.activity;
 
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 import edu.udemy.android.whatsapp_clone.R;
+import edu.udemy.android.whatsapp_clone.config.FirebaseConfig;
+import edu.udemy.android.whatsapp_clone.model.User;
 
 public class RegisterUserActivity extends AppCompatActivity {
+    private EditText edt_registerName;
+    private EditText edt_registerEmail;
+    private EditText edt_registerPassword;
+
+    private Button btn_registerRegister;
+
+    private User user;
+    private FirebaseAuth firebaseAuth;
 
     @Override
     protected void onCreate( Bundle savedInstanceState ) {
         super.onCreate( savedInstanceState );
         setContentView( R.layout.activity_register_user );
+
+        createComponents();
+
+        btn_registerRegister.setOnClickListener( new View.OnClickListener() {
+            @Override
+            public void onClick( View v ) {
+                registerUser();
+            }
+        } );
+    }
+
+    /**
+     * Register in the database a new user.
+     */
+    private void registerUser() {
+        createUser();
+
+        firebaseAuth = FirebaseConfig.getFirebaseAuthentication();
+
+        firebaseAuth.createUserWithEmailAndPassword(
+                user.getEmail(),
+                user.getPassword()
+        ).addOnCompleteListener( RegisterUserActivity.this, new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete( @NonNull Task<AuthResult> task ) {
+                if( task.isSuccessful() ) {
+                    Toast.makeText(
+                            RegisterUserActivity.this, "User registered", Toast.LENGTH_SHORT )
+                            .show();
+                } else {
+                    Toast.makeText(
+                            RegisterUserActivity.this, "User not registered", Toast.LENGTH_SHORT )
+                            .show();
+                }
+            }
+        } );
+    }
+
+    /**
+     * Create, from the editable data, a new user.
+     */
+    private void createUser() {
+        user = new User();
+        user.setName( edt_registerName.getText().toString() );
+        user.setEmail( edt_registerEmail.getText().toString() );
+        user.setPassword( edt_registerPassword.getText().toString() );
+    }
+
+    /**
+     * Create references to components of user interface.
+     */
+    private void createComponents() {
+        edt_registerName = (EditText) findViewById( R.id.edt_registerName );
+        edt_registerEmail = (EditText) findViewById( R.id.edt_registerEmail );
+        edt_registerPassword = (EditText) findViewById( R.id.edt_registerPassword );
+
+        btn_registerRegister = (Button) findViewById( R.id.btn_registerRegister );
     }
 }
--- Whatsapp_clone/app/src/main/java/edu/udemy/android/whatsapp_clone/config/FirebaseConfig.java
+++ Whatsapp_clone/app/src/main/java/edu/udemy/android/whatsapp_clone/config/FirebaseConfig.java
@@ -1,10 +1,12 @@
 package edu.udemy.android.whatsapp_clone.config;
 
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
 public final class FirebaseConfig {
     private static DatabaseReference firebaseReference;
+    private static FirebaseAuth firebaseAuthentication;
 
     public static DatabaseReference getFirebase() {
         if( firebaseReference == null ) {
@@ -12,5 +14,13 @@ public final class FirebaseConfig {
         }
 
         return firebaseReference;
+    }
+
+    public static FirebaseAuth getFirebaseAuthentication() {
+        if( firebaseAuthentication == null ) {
+            firebaseAuthentication = FirebaseAuth.getInstance();
+        }
+
+        return firebaseAuthentication;
     }
 }
