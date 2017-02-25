a6f4b55b59839d86c7889fc2936f2e71cfe33220
TomazMartins <tomaz.r.martins@gmail.com>
2017-02-25 01:14:08 -0300

    Creating the Register Layout
    Signed-off-by: TomazMartins <tomaz.r.martins@gmail.com>

---
+++ Whatsapp_clone/app/src/main/java/edu/udemy/android/whatsapp_clone/activity/RegisterUserActivity.java
@@ -0,0 +1,15 @@
+package edu.udemy.android.whatsapp_clone.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import edu.udemy.android.whatsapp_clone.R;
+
+public class RegisterUserActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate( Bundle savedInstanceState ) {
+        super.onCreate( savedInstanceState );
+        setContentView( R.layout.activity_register_user );
+    }
+}
