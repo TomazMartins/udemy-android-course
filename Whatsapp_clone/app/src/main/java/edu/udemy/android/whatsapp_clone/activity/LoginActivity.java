f00fa2d6dfee5b63eeec9f214bb73d7c7bec60e9
TomazMartins <tomaz.r.martins@gmail.com>
2017-02-26 22:20:16 -0300

    Implementing Login methods
    Signed-off-by: TomazMartins <tomaz.r.martins@gmail.com>

--- Whatsapp_clone/app/src/main/java/edu/udemy/android/whatsapp_clone/activity/LoginActivity.java
+++ Whatsapp_clone/app/src/main/java/edu/udemy/android/whatsapp_clone/activity/LoginActivity.java
@@ -14,10 +14,16 @@ import android.widget.EditText;
 
 import com.github.rtoshiro.util.format.SimpleMaskFormatter;
 import com.github.rtoshiro.util.format.text.MaskTextWatcher;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 import edu.udemy.android.whatsapp_clone.R;
+import edu.udemy.android.whatsapp_clone.config.FirebaseConfig;
 import edu.udemy.android.whatsapp_clone.helper.Permission;
 import edu.udemy.android.whatsapp_clone.helper.Preferences;
+import edu.udemy.android.whatsapp_clone.model.User;
 
 import android.widget.Button;
 import android.widget.Toast;
@@ -31,12 +37,56 @@ public class LoginActivity extends AppCompatActivity {
 
     private Button btn_loginLogin;
 
+    private FirebaseAuth firebaseAuthentication;
+    private User user;
+
     @Override
     protected void onCreate( Bundle savedInstanceState ) {
         super.onCreate( savedInstanceState );
         setContentView( R.layout.activity_login );
 
         createComponents();
+
+        btn_loginLogin.setOnClickListener( new View.OnClickListener() {
+            @Override
+            public void onClick( View v ) {
+                validateUser();
+            }
+        } );
+    }
+
+
+    /**
+     * Validate the login of the user. If the user exits, there is successful
+     *   in login. If not, the user won't can do the login.
+     */
+    private void validateUser() {
+        setUser();
+
+        firebaseAuthentication = FirebaseConfig.getFirebaseAuthentication();
+        firebaseAuthentication.signInWithEmailAndPassword(
+                user.getEmail(), user.getPassword()
+        ).addOnCompleteListener( new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete( @NonNull Task<AuthResult> task ) {
+                if( task.isSuccessful() ) {
+                    Toast.makeText( LoginActivity.this, "Success in Login", Toast.LENGTH_SHORT )
+                            .show();
+                } else {
+                    Toast.makeText( LoginActivity.this, "User didn't exist", Toast.LENGTH_SHORT )
+                            .show();
+                }
+            }
+        } );
+    }
+
+    /**
+     * Configure the user with the collected data from the user.
+     */
+    private void setUser() {
+        user = new User();
+        user.setEmail( edt_loginEmail.getText().toString() );
+        user.setPassword( edt_loginPassword.getText().toString() );
     }
 
     /**
