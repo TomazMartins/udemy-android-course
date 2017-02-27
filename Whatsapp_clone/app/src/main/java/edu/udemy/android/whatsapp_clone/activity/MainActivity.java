dc36b05458dd03294a55e5cb86f89b12df1a1cec
TomazMartins <tomaz.r.martins@gmail.com>
2017-02-27 11:38:19 -0300

    Creating Menu in Toolbar
    Signed-off-by: TomazMartins <tomaz.r.martins@gmail.com>

--- Whatsapp_clone/app/src/main/java/edu/udemy/android/whatsapp_clone/activity/MainActivity.java
+++ Whatsapp_clone/app/src/main/java/edu/udemy/android/whatsapp_clone/activity/MainActivity.java
@@ -2,14 +2,14 @@ package edu.udemy.android.whatsapp_clone.activity;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-
-import com.google.firebase.auth.FirebaseAuth;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
 
 import edu.udemy.android.whatsapp_clone.R;
 
 public class MainActivity extends AppCompatActivity {
-
-    private FirebaseAuth firebaseAuth;
+    private Toolbar toolbar;
 
     @Override
     protected void onCreate( Bundle savedInstanceState ) {
@@ -19,6 +19,28 @@ public class MainActivity extends AppCompatActivity {
         createComponents();
     }
 
+    @Override
+    public boolean onCreateOptionsMenu( Menu menu ) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate( R.menu.menu_main, menu );
+
+        return true;
+    }
+
+    /**
+     * Create references to components of user interface.
+     */
     private void createComponents() {
+        toolbar = (Toolbar) findViewById( R.id.toolbar );
+
+        setToolbar();
+    }
+
+    /**
+     * Configure the Toolbar
+     */
+    private void setToolbar() {
+        toolbar.setTitle( "Whatsapp" );
+        setSupportActionBar( toolbar );
     }
 }
