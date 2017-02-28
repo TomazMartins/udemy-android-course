60e9d587313bdadca2f77d48e54aa558f5020e5e
TomazMartins <tomaz.r.martins@gmail.com>
2017-02-28 10:08:54 -0300

    Initial commit InstagramClone
    1.  Adding SDK Parse for Android
    2.  Initial Configure to use the Back4App with
        the SDK Parse.
    2.1 This configuration is in the method onCreate()
        of the StarterApplication class.
    
    Signed-off-by: TomazMartins <tomaz.r.martins@gmail.com>

---
+++ InstagramClone/ParseStarterProject/src/main/java/com/parse/starter/StarterApplication.java
@@ -0,0 +1,59 @@
+/*
+ * Copyright (c) 2015-present, Parse, LLC.
+ * All rights reserved.
+ *
+ * This source code is licensed under the BSD-style license found in the
+ * LICENSE file in the root directory of this source tree. An additional grant
+ * of patent rights can be found in the PATENTS file in the same directory.
+ */
+package com.parse.starter;
+
+import android.app.Application;
+import android.util.Log;
+
+import com.parse.Parse;
+import com.parse.ParseACL;
+import com.parse.ParseException;
+import com.parse.ParseObject;
+import com.parse.ParseUser;
+import com.parse.SaveCallback;
+
+
+public class StarterApplication extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        // Enable Local Datastore.
+        Parse.enableLocalDatastore( this );
+
+        // Add your initialization code here
+        Parse.initialize( new Parse.Configuration.Builder( getApplicationContext() )
+                .applicationId( "mUmfPGYFUyLkv9fzrlPFDFPLHjC8jIiVjbKnCyvD" )
+                .clientKey( "Jfgq3f16g1gHDlntOJDC3JB2Cnsj3geO7vNJjFlV" )
+                .server( "https://parseapi.back4app.com/" )
+                .build()
+        );
+
+        ParseObject punctuation = new ParseObject( "Punctuation" );
+        punctuation.put( "points", 100 );
+
+        punctuation.saveInBackground( new SaveCallback() {
+            @Override
+            public void done( ParseException e ) {
+                if( e == null ) {
+                    Log.i( "TESTE EXECUTION", "Save with success" );
+                } else {
+                    Log.i( "TESTE EXECUTION", "Save failed" );
+                }
+            }
+        } );
+
+        ParseUser.enableAutomaticUser();
+        ParseACL defaultACL = new ParseACL();
+        // Optionally enable public read access.
+        // defaultACL.setPublicReadAccess(true);
+        ParseACL.setDefaultACL( defaultACL, true );
+    }
+}
