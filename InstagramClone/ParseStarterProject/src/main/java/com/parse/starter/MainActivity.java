6f73acbbb76b5317a4a224277bdd6a2fb0a93124
TomazMartins <tomaz.r.martins@gmail.com>
2017-03-01 10:42:47 -0300

    Implementing method of update points with Parse class
    Signed-off-by: TomazMartins <tomaz.r.martins@gmail.com>

--- InstagramClone/ParseStarterProject/src/main/java/com/parse/starter/MainActivity.java
+++ InstagramClone/ParseStarterProject/src/main/java/com/parse/starter/MainActivity.java
@@ -9,16 +9,17 @@
 package com.parse.starter;
 
 import android.os.Bundle;
-import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
-import android.view.Menu;
-import android.view.MenuItem;
 
-import com.parse.ParseAnalytics;
+import com.parse.FindCallback;
+import com.parse.GetCallback;
 import com.parse.ParseException;
 import com.parse.ParseObject;
+import com.parse.ParseQuery;
 import com.parse.SaveCallback;
+
+import java.util.List;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -28,9 +29,49 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate( savedInstanceState );
         setContentView( R.layout.activity_main );
 
+        updatePoints( "Tom", 200 );
+    }
+
+
+    /**
+     * Update the punctuation of someone with Parse class.
+     *
+     * @param name name of someone who do the punctuation.
+     * @param points the new punctuation.
+     */
+    private void updatePoints( final String name, final int points ) {
+
+        ParseQuery<ParseObject> parseQuery = ParseQuery.getQuery( "Punctuation" );
+        parseQuery.findInBackground( new FindCallback<ParseObject>() {
+            @Override
+            public void done( List<ParseObject> objects, ParseException e ) {
+                if( e == null ) {
+                    for( ParseObject object : objects ) {
+                        if( object.getString( "name" ).equals( name ) ) {
+                            object.put( "punctuation", points );
+                            object.saveInBackground();
+                        }
+                    }
+
+                    Log.i( "UPDATE POINTS", "Update data with success" );
+                } else {
+                    Log.i( "UPDATE POINTS", "Fail while update data" );
+                }
+            }
+        } );
+    }
+
+    /**
+     * Save the punctuation of someone with Parse class.
+     *
+     * @param name   name of someone who do the punctuation.
+     * @param points total points of someone.
+     */
+    private void savePoints( String name, int points ) {
         ParseObject punctuation = new ParseObject( "Punctuation" );
-        punctuation.put( "name", "Mário" );
-        punctuation.put( "punctuation", 800 );
+        punctuation.put( "name", name );
+        punctuation.put( "punctuation", points );
+
         punctuation.saveInBackground( new SaveCallback() {
             @Override
             public void done( ParseException pe ) {
--- InstagramClone/ParseStarterProject/src/main/java/com/parse/starter/StarterApplication.java
+++ InstagramClone/ParseStarterProject/src/main/java/com/parse/starter/StarterApplication.java
@@ -38,8 +38,8 @@ public class StarterApplication extends Application {
 
 //        ParseUser.enableAutomaticUser();
         ParseACL defaultACL = new ParseACL();
-        // Optionally enable public read access.
-        // defaultACL.setPublicReadAccess(true);
-        ParseACL.setDefaultACL( defaultACL, true );
+
+        defaultACL.setPublicReadAccess( true );
+//        ParseACL.setDefaultACL( defaultACL, true );
     }
 }
